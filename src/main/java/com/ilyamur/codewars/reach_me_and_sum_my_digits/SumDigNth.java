31f82e0381c4e25d45165a376e3251e729b04be0
Ilya Muravyev <Ilya_Muravyev@epam.com>
2016-06-02 18:44:37 +0300

    -

--- src/main/java/com/ilyamur/codewars/reach_me_and_sum_my_digits/SumDigNth.java
+++ src/main/java/com/ilyamur/codewars/reach_me_and_sum_my_digits/SumDigNth.java
@@ -3,6 +3,16 @@ package com.ilyamur.codewars.reach_me_and_sum_my_digits;
 public class SumDigNth {
 
     public static long sumDigNthTerm(long initval, long[] patternl, int nthterm) {
-        return 0L;
+        long acc = initval;
+        for (int i = 0; i < nthterm - 1; i++) {
+            acc += patternl[i % patternl.length];
+        }
+        return sumDigits(acc);
+    }
+
+    private static long sumDigits(long n) {
+        long curr = n % 10;
+        long next = n / 10;
+        return (next > 0) ? curr + sumDigits(next) : curr;
     }
 }
--- src/test/java/com/ilyamur/codewars/reach_me_and_sum_my_digits/SumDigNthTest.java
+++ src/test/java/com/ilyamur/codewars/reach_me_and_sum_my_digits/SumDigNthTest.java
@@ -6,17 +6,13 @@ import org.junit.Test;
 
 public class SumDigNthTest {
 
-    private static void testing(long act, long exp) {
-        assertEquals(exp, act);
-    }
-
     @Test
     public void test1() {
         System.out.println("Basic Tests **** sumDigNthTerm");
-        testing(SumDigNth.sumDigNthTerm(10, new long[]{2, 1, 3}, 6), 10);
-        testing(SumDigNth.sumDigNthTerm(10, new long[]{2, 1, 3}, 15), 10);
-        testing(SumDigNth.sumDigNthTerm(10, new long[]{2, 1, 3}, 50), 9);
-        testing(SumDigNth.sumDigNthTerm(10, new long[]{2, 1, 3}, 78), 10);
-        testing(SumDigNth.sumDigNthTerm(10, new long[]{2, 1, 3}, 157), 7);
+        assertEquals(10, SumDigNth.sumDigNthTerm(10, new long[]{2, 1, 3}, 6));
+        assertEquals(10, SumDigNth.sumDigNthTerm(10, new long[]{2, 1, 3}, 15));
+        assertEquals(9, SumDigNth.sumDigNthTerm(10, new long[]{2, 1, 3}, 50));
+        assertEquals(10, SumDigNth.sumDigNthTerm(10, new long[]{2, 1, 3}, 78));
+        assertEquals(7, SumDigNth.sumDigNthTerm(10, new long[]{2, 1, 3}, 157));
     }
 }
