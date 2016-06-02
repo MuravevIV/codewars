15a204778f119f979b2348c423d51946c7f8bdef
Ilya Muravyev <Ilya_Muravyev@epam.com>
2016-06-02 18:28:58 +0300

    -

---
+++ src/main/java/com/ilyamur/codewars/reach_me_and_sum_my_digits/SumDigNth.java
@@ -0,0 +1,8 @@
+package com.ilyamur.codewars.reach_me_and_sum_my_digits;
+
+public class SumDigNth {
+
+    public static long sumDigNthTerm(long initval, long[] patternl, int nthterm) {
+        return 0L;
+    }
+}
---
+++ src/test/java/com/ilyamur/codewars/reach_me_and_sum_my_digits/SumDigNthTest.java
@@ -0,0 +1,22 @@
+package com.ilyamur.codewars.reach_me_and_sum_my_digits;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SumDigNthTest {
+
+    private static void testing(long act, long exp) {
+        assertEquals(exp, act);
+    }
+
+    @Test
+    public void test1() {
+        System.out.println("Basic Tests **** sumDigNthTerm");
+        testing(SumDigNth.sumDigNthTerm(10, new long[]{2, 1, 3}, 6), 10);
+        testing(SumDigNth.sumDigNthTerm(10, new long[]{2, 1, 3}, 15), 10);
+        testing(SumDigNth.sumDigNthTerm(10, new long[]{2, 1, 3}, 50), 9);
+        testing(SumDigNth.sumDigNthTerm(10, new long[]{2, 1, 3}, 78), 10);
+        testing(SumDigNth.sumDigNthTerm(10, new long[]{2, 1, 3}, 157), 7);
+    }
+}
