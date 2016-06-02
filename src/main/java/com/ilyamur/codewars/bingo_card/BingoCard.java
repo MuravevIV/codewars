070b063a7fc23c7b08d1a96410ba0949dcc0d3fc
Ilya Muravyev <Ilya_Muravyev@epam.com>
2016-06-02 18:20:49 +0300

    -

--- src/main/java/com/ilyamur/codewars/bingo_card/BingoCard.java
+++ src/main/java/com/ilyamur/codewars/bingo_card/BingoCard.java
@@ -1,8 +1,5 @@
 package com.ilyamur.codewars.bingo_card;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -10,34 +7,33 @@ import java.util.Random;
 
 public class BingoCard {
 
-    private static final Logger LOG = LoggerFactory.getLogger(BingoCard.class);
-
+    private static final char[] LETTERS = new char[]{'B', 'I', 'N', 'G', 'O'};
+    private static final int OFFSET = 15;
     private static Random RANDOM = new Random();
 
     public static String[] getCard() {
         Map<Integer, Integer> misfits = new HashMap<>();
-        char[] letters = new char[] {'B', 'I', 'N', 'G', 'O'};
-        ArrayList<String> objects = new ArrayList<>(24);
-        for (int col = 0; col < 5; col++) {
-            char letter = letters[col];
-            long rowCount = (letter == 'N') ? 4 : 5;
-            int misfitPosition = 15 * (col + 1);
+        ArrayList<String> objects = new ArrayList<>();
+        for (int col = 0; col < LETTERS.length; col++) {
+            char letter = LETTERS[col];
+            int misfitPos = OFFSET * (col + 1);
+            long rowCount = getRowCount(letter);
             for (int row = 0; row < rowCount; row++) {
-                int numPosition = 15 * col + 1 + RANDOM.nextInt(15 - row);
-                Integer numValue = getNumValue(misfits, numPosition);
-                misfits.put(numPosition,  getNumValue(misfits, misfitPosition));
-                LOG.info("add {}", numValue);
-                objects.add(Character.toString(letter) + numValue);
-                if (numValue == 58) {
-                    System.out.println(numValue);
-                }
-                misfitPosition--;
+                int randomPos = OFFSET * col + 1 + RANDOM.nextInt(OFFSET - row);
+                Integer number = getNumber(misfits, randomPos);
+                misfits.put(randomPos, getNumber(misfits, misfitPos));
+                objects.add(Character.toString(letter) + number);
+                misfitPos--;
             }
         }
-        return objects.toArray(new String[24]);
+        return objects.toArray(new String[objects.size()]);
     }
 
-    private static Integer getNumValue(Map<Integer, Integer> misfits, int numPosition) {
+    private static int getRowCount(char letter) {
+        return (letter == 'N') ? 4 : 5;
+    }
+
+    private static Integer getNumber(Map<Integer, Integer> misfits, int numPosition) {
         Integer misfitValue = misfits.get(numPosition);
         return (misfitValue != null) ? misfitValue : numPosition;
     }
